52915160d8d2ae21247891cfaec7d8e2001687ee
Egor Merk <egor@merkeg.de>
2024-07-12 20:49:11 +0200

    logging

--- src/main/java/de/merkeg/oauth2/data/OAuth2Response.java
+++ src/main/java/de/merkeg/oauth2/data/OAuth2Response.java
@@ -3,9 +3,11 @@ package de.merkeg.oauth2.data;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class OAuth2Response {
     @JsonProperty("access_token")
     String accessToken;
--- src/main/java/de/merkeg/oauth2/data/OAuth2TwitchResponse.java
+++ src/main/java/de/merkeg/oauth2/data/OAuth2TwitchResponse.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class OAuth2TwitchResponse {
 
     @JsonProperty("access_token")
