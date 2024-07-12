9ed735c79b5a7a27bd987ba0600d945cef4d2d2f
Egor Merk <egor@merkeg.de>
2024-07-12 19:31:35 +0200

    Change media type and added Metrics

--- src/main/java/de/merkeg/oauth2/OAuth2Resource.java
+++ src/main/java/de/merkeg/oauth2/OAuth2Resource.java
@@ -5,12 +5,18 @@ import de.merkeg.oauth2.data.OAuth2Request;
 import de.merkeg.oauth2.data.OAuth2Response;
 import de.merkeg.oauth2.data.OAuth2ResponseMapper;
 import de.merkeg.oauth2.data.OAuth2TwitchResponse;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.print.attribute.standard.Media;
 
 @Path("/oauth2")
 public class OAuth2Resource {
@@ -22,9 +28,16 @@ public class OAuth2Resource {
     @Inject
     OAuth2ResponseMapper mapper;
 
+    @Inject
+    MeterRegistry meterRegistry;
+
     @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
     @Path("token")
     public OAuth2Response token(OAuth2Request request) {
+        meterRegistry.counter("http_request_token_counter",
+                Tags.of(Tag.of("clientId", request.getClientId()), Tag.of("grantType", request.getGrantType()))).increment();
 
         OAuth2TwitchResponse response = twitchApiClient.tokenExchange(request);
 
