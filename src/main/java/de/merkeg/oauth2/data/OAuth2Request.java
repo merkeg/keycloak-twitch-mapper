abedb187f648afb81b753614a5a7da80e6768634
Egor Merk <egor@merkeg.de>
2024-07-12 20:15:10 +0200

    fix mistake

--- src/main/java/de/merkeg/oauth2/data/OAuth2Request.java
+++ src/main/java/de/merkeg/oauth2/data/OAuth2Request.java
@@ -1,6 +1,7 @@
 package de.merkeg.oauth2.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.ws.rs.FormParam;
 import lombok.Getter;
 import lombok.Setter;
 import org.jboss.resteasy.reactive.RestForm;
@@ -9,23 +10,23 @@ import org.jboss.resteasy.reactive.RestForm;
 @Setter
 public class OAuth2Request {
 
-    @RestForm
+    @FormParam("client_id")
     @JsonProperty("client_id")
     String clientId;
 
-    @RestForm
+    @FormParam("client_secret")
     @JsonProperty("client_secret")
     String clientSecret;
 
-    @RestForm
+    @FormParam("code")
     @JsonProperty("code")
     String code;
 
-    @RestForm
+    @FormParam("grant_type")
     @JsonProperty("grant_type")
     String grantType;
 
-    @RestForm
+    @FormParam("redirect_uri")
     @JsonProperty("redirect_uri")
     String redirectUri;
 }
