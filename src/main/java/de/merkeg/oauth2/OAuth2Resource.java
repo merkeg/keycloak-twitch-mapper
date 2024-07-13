f1727915a192acf1ed69c01a1daf6c8fe7ca2899
Egor Merk <egor@merkeg.de>
2024-07-13 07:33:13 +0200

    logging

--- src/main/java/de/merkeg/oauth2/OAuth2Resource.java
+++ src/main/java/de/merkeg/oauth2/OAuth2Resource.java
@@ -1,5 +1,7 @@
 package de.merkeg.oauth2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.merkeg.TwitchApiClient;
 import de.merkeg.oauth2.data.OAuth2Request;
 import de.merkeg.oauth2.data.OAuth2Response;
@@ -8,6 +10,7 @@ import de.merkeg.oauth2.data.OAuth2TwitchResponse;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.Tags;
+import io.quarkus.vertx.http.runtime.devmode.Json;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -34,13 +37,17 @@ public class OAuth2Resource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED})
     @Path("token")
-    public OAuth2Response token(@BeanParam OAuth2Request request) {
+    public OAuth2Response token(@BeanParam OAuth2Request request) throws JsonProcessingException {
         log.debug("New token mapping request");
         OAuth2TwitchResponse response = twitchApiClient.tokenExchange(request);
         log.debug("Got twitch response data: {}", response);
 
         OAuth2Response mapped = mapper.map(response);
         log.debug("Got mapped response data: {}", mapped);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        log.debug(objectMapper.writeValueAsString(mapped));
+
         return mapped;
     }
 }
